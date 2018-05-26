320d4653131c460bd3994fd92e683f4f61cb4831
Dhruva Shaw <34207400+Dhruvacube@users.noreply.github.com>
2018-05-26 16:15:51 +0530

    Add files via upload

---
+++ outof20.js
@@ -0,0 +1,467 @@
+/*Made By Dhruva Shaw
+  All Rights Reserved
+  It shall not be copied without prior permission of the owner of the website. */  
+
+
+//JavaScript Code for marks out of 10
+//Science
+function getgrades20() {
+  var marks = document.getElementById("marksin120").value;
+  switch(marks)
+{
+    case "0":
+    case "0.5":
+    case "1":
+    case "1.5":
+    case "2":
+    case "2.5":
+    case "3":
+    case "3.5":
+    case "4":
+       {
+        document.getElementById("grades20").innerHTML="Your Grade is E1";
+        break;
+       }
+    case "4.5":
+    case "5":
+    case "5.5":
+    case "6":
+     {
+      document.getElementById("grades20").innerHTML="Your Grade is E1";
+      break;
+     }
+    case "6.5":
+    case "7":
+    case "7.5":
+    case "8":
+    {
+     document.getElementById("grades20").innerHTML="Your Grade is D";
+      break;
+    }
+    case "8.5":
+    case "9":
+    case "9.5":
+    case "10":
+     {
+      document.getElementById("grades20").innerHTML="Your Grade is C2";
+      break;
+     }
+    case "10.5":
+    case "11":
+    case "11.5":
+    case "12":
+     {
+      document.getElementById("grades20").innerHTML="Your Grade is C1";
+      break;
+     }
+    case "12.5":
+    case "13":
+    case "13.5":
+    case "14":
+     {
+      document.getElementById("grades20").innerHTML= "Your Grade is B2";
+      break;
+     }
+    case "14.5":
+    case "15":
+    case "15.5":
+    case "16":
+    {
+      document.getElementById("grades20").innerHTML="Your Grade is B1";
+      break;
+    }
+    case "16.5":
+    case "17":
+    case "17.5":
+    case "18":
+     {
+      document.getElementById("grades20").innerHTML="Your Grade is A2";
+      break;
+      }
+    case "18.5":
+    case "19":
+    case "19.5":
+    case "20":
+     {
+      document.getElementById("grades20").innerHTML= "Your Grade is A1";
+      break;
+     }
+
+    default:
+     {
+      document.getElementById("grades20").innerHTML="Invalid Input !!!!";
+      break;
+    }
+}
+}
+ 
+//Hindi
+ function getgradesone20() {
+  var marks = document.getElementById("marksin220").value;
+  switch(marks)
+{
+    case "0":
+    case "0.5":
+    case "1":
+    case "1.5":
+    case "2":
+    case "2.5":
+    case "3":
+    case "3.5":
+    case "4":
+       {
+        document.getElementById("grades120").innerHTML="Your Grade is E1";
+        break;
+       }
+    case "4.5":
+    case "5":
+    case "5.5":
+    case "6":
+     {
+      document.getElementById("grades120").innerHTML="Your Grade is E1";
+      break;
+     }
+    case "6.5":
+    case "7":
+    case "7.5":
+    case "8":
+    {
+     document.getElementById("grades120").innerHTML="Your Grade is D";
+      break;
+    }
+    case "8.5":
+    case "9":
+    case "9.5":
+    case "10":
+     {
+      document.getElementById("grades120").innerHTML="Your Grade is C2";
+      break;
+     }
+    case "10.5":
+    case "11":
+    case "11.5":
+    case "12":
+     {
+      document.getElementById("grades120").innerHTML="Your Grade is C1";
+      break;
+     }
+    case "12.5":
+    case "13":
+    case "13.5":
+    case "14":
+     {
+      document.getElementById("grades120").innerHTML= "Your Grade is B2";
+      break;
+     }
+    case "14.5":
+    case "15":
+    case "15.5":
+    case "16":
+    {
+      document.getElementById("grades120").innerHTML="Your Grade is B1";
+      break;
+    }
+    case "16.5":
+    case "17":
+    case "17.5":
+    case "18":
+     {
+      document.getElementById("grades120").innerHTML="Your Grade is A2";
+      break;
+      }
+    case "18.5":
+    case "19":
+    case "19.5":
+    case "20":
+     {
+      document.getElementById("grades120").innerHTML= "Your Grade is A1";
+      break;
+     }
+
+    default:
+     {
+      document.getElementById("grades120").innerHTML="Invalid Input !!!!";
+      break;
+    }
+}
+}
+
+//Maths
+function getgradestwo20() {
+  var marks = document.getElementById("marksin320").value;
+  switch(marks)
+{
+    case "0":
+    case "0.5":
+    case "1":
+    case "1.5":
+    case "2":
+    case "2.5":
+    case "3":
+    case "3.5":
+    case "4":
+       {
+        document.getElementById("grades220").innerHTML="Your Grade is E1";
+        break;
+       }
+    case "4.5":
+    case "5":
+    case "5.5":
+    case "6":
+     {
+      document.getElementById("grades220").innerHTML="Your Grade is E1";
+      break;
+     }
+    case "6.5":
+    case "7":
+    case "7.5":
+    case "8":
+    {
+     document.getElementById("grades220").innerHTML="Your Grade is D";
+      break;
+    }
+    case "8.5":
+    case "9":
+    case "9.5":
+    case "10":
+     {
+      document.getElementById("grades220").innerHTML="Your Grade is C2";
+      break;
+     }
+    case "10.5":
+    case "11":
+    case "11.5":
+    case "12":
+     {
+      document.getElementById("grades220").innerHTML="Your Grade is C1";
+      break;
+     }
+    case "12.5":
+    case "13":
+    case "13.5":
+    case "14":
+     {
+      document.getElementById("grades220").innerHTML= "Your Grade is B2";
+      break;
+     }
+    case "14.5":
+    case "15":
+    case "15.5":
+    case "16":
+    {
+      document.getElementById("grades220").innerHTML="Your Grade is B1";
+      break;
+    }
+    case "16.5":
+    case "17":
+    case "17.5":
+    case "18":
+     {
+      document.getElementById("grades220").innerHTML="Your Grade is A2";
+      break;
+      }
+    case "18.5":
+    case "19":
+    case "19.5":
+    case "20":
+     {
+      document.getElementById("grades220").innerHTML= "Your Grade is A1";
+      break;
+     }
+
+    default:
+     {
+      document.getElementById("grades220").innerHTML="Invalid Input !!!!";
+      break;
+    }
+}
+}
+
+//S.S.T
+function getgradesthree20() {
+  var marks = document.getElementById("marksin420").value;
+  switch(marks)
+{
+    case "0":
+    case "0.5":
+    case "1":
+    case "1.5":
+    case "2":
+    case "2.5":
+    case "3":
+    case "3.5":
+    case "4":
+       {
+        document.getElementById("grades320").innerHTML="Your Grade is E1";
+        break;
+       }
+    case "4.5":
+    case "5":
+    case "5.5":
+    case "6":
+     {
+      document.getElementById("grades320").innerHTML="Your Grade is E1";
+      break;
+     }
+    case "6.5":
+    case "7":
+    case "7.5":
+    case "8":
+    {
+     document.getElementById("grades320").innerHTML="Your Grade is D";
+      break;
+    }
+    case "8.5":
+    case "9":
+    case "9.5":
+    case "10":
+     {
+      document.getElementById("grades320").innerHTML="Your Grade is C2";
+      break;
+     }
+    case "10.5":
+    case "11":
+    case "11.5":
+    case "12":
+     {
+      document.getElementById("grades320").innerHTML="Your Grade is C1";
+      break;
+     }
+    case "12.5":
+    case "13":
+    case "13.5":
+    case "14":
+     {
+      document.getElementById("grades320").innerHTML= "Your Grade is B2";
+      break;
+     }
+    case "14.5":
+    case "15":
+    case "15.5":
+    case "16":
+    {
+      document.getElementById("grades320").innerHTML="Your Grade is B1";
+      break;
+    }
+    case "16.5":
+    case "17":
+    case "17.5":
+    case "18":
+     {
+      document.getElementById("grades320").innerHTML="Your Grade is A2";
+      break;
+      }
+    case "18.5":
+    case "19":
+    case "19.5":
+    case "20":
+     {
+      document.getElementById("grades320").innerHTML= "Your Grade is A1";
+      break;
+     }
+
+    default:
+     {
+      document.getElementById("grades320").innerHTML="Invalid Input !!!!";
+      break;
+    }
+}
+}
+ 
+//English
+ function getgradesfour20() {
+  var marks = document.getElementById("marksin520").value;
+  switch(marks)
+{
+    case "0":
+    case "0.5":
+    case "1":
+    case "1.5":
+    case "2":
+    case "2.5":
+    case "3":
+    case "3.5":
+    case "4":
+       {
+        document.getElementById("grades420").innerHTML="Your Grade is E1";
+        break;
+       }
+    case "4.5":
+    case "5":
+    case "5.5":
+    case "6":
+     {
+      document.getElementById("grades420").innerHTML="Your Grade is E1";
+      break;
+     }
+    case "6.5":
+    case "7":
+    case "7.5":
+    case "8":
+    {
+     document.getElementById("grades420").innerHTML="Your Grade is D";
+      break;
+    }
+    case "8.5":
+    case "9":
+    case "9.5":
+    case "10":
+     {
+      document.getElementById("grades420").innerHTML="Your Grade is C2";
+      break;
+     }
+    case "10.5":
+    case "11":
+    case "11.5":
+    case "12":
+     {
+      document.getElementById("grades420").innerHTML="Your Grade is C1";
+      break;
+     }
+    case "12.5":
+    case "13":
+    case "13.5":
+    case "14":
+     {
+      document.getElementById("grades420").innerHTML= "Your Grade is B2";
+      break;
+     }
+    case "14.5":
+    case "15":
+    case "15.5":
+    case "16":
+    {
+      document.getElementById("grades420").innerHTML="Your Grade is B1";
+      break;
+    }
+    case "16.5":
+    case "17":
+    case "17.5":
+    case "18":
+     {
+      document.getElementById("grades420").innerHTML="Your Grade is A2";
+      break;
+      }
+    case "18.5":
+    case "19":
+    case "19.5":
+    case "20":
+     {
+      document.getElementById("grades420").innerHTML= "Your Grade is A1";
+      break;
+     }
+
+    default:
+     {
+      document.getElementById("grades420").innerHTML="Invalid Input !!!!";
+      break;
+    }
+}
+}
+ 
+ 
+ 
+
+
+
+
