25650222f255ff6657c3dc04b5b0bf81b532e06a
Dhruva Shaw <34207400+Dhruvacube@users.noreply.github.com>
2018-05-29 08:26:54 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+/*Made By Dhruva Shaw
+  All Rights Reserved
+  It shall not be copied without prior permission of the owner of the website. */
+
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+                                  if (event.target == modal) {
+                                                               modal.style.display = "none";
+                                                             }
+                                 }
+
