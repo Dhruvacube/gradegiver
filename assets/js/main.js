ac0e6160529714c23f479a8c979c594e8c58627b
Dhruva Shaw <34207400+Dhruvacube@users.noreply.github.com>
2018-05-29 16:46:15 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,10 +6,30 @@
 // Get the modal
 var modal = document.getElementById('id01');
 
+
+
+/* For Scroll to top button (Javascript) */
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
                                   if (event.target == modal) {
                                                                modal.style.display = "none";
                                                              }
                                  }
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+                           if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                           document.getElementById("myBtn").style.display = "block";
+                                                                                                        } else {
+                                                                                                                document.getElementById("myBtn").style.display = "none";
+                                                                                                                }
+                           }
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+                        document.body.scrollTop = 0; // For Safari
+                        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+                       }
 
